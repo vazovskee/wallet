cba9970932f9d6d6ad1620f41552ee433270411d
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-21 16:37:47 +0300

    function for transferring without commission was added

--- Wallet.sol
+++ Wallet.sol
@@ -9,9 +9,15 @@ contract Wallet {
         tvm.accept();
     }
 
+    // модификатор, подтверждающий сообщения, принадлежащие только владельцу кошелька
     modifier checkOwnerAndAccept {
         require(msg.pubkey() == tvm.pubkey(), 100);
 		tvm.accept();
 		_;
 	}
+
+    // функция для отправки средств без оплаты комиссии за свой счёт
+    function sendValueWithoutCommission(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, true, 0);
+    }
 }
