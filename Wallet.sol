ff1fc796a15009f1d18dc8457654bf038728e11c
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-21 16:48:27 +0300

    function for transferring all value with wallet destruction was added

--- Wallet.sol
+++ Wallet.sol
@@ -25,4 +25,9 @@ contract Wallet {
     function sendValueWithCommission(address dest, uint128 value) public pure checkOwnerAndAccept {
         dest.transfer(value, true, 1);  //  flag = 1 (sender pays transfer fees separately from contract's balance)
     }
+
+    // функция для отправки всех средств и уничтожения кошелька
+    function sendAllValueAndDestroyWallet(address dest) public pure checkOwnerAndAccept {
+        dest.transfer(0, true, 160);  //  flag = 160 (send all balance and destroy the contract)
+    }
 }
